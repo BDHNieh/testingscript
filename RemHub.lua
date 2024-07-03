420147d3d91940d400cc4aec60898e3ca07aa8a1
BDHNieh <bdhculac832457@gmail.com>
2024-07-03 21:27:30 +0700

    a

---
+++ RemHub.lua
@@ -0,0 +1,3 @@
+if game.PlaceId == 
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
