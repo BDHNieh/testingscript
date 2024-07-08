4cddc355978696f83916d88d2fa36cdc6535c943
BDHNieh <bdhculac832457@gmail.com>
2024-07-08 17:31:24 +0700

    a

--- RemHub.lua
+++ RemHub.lua
@@ -1,29 +1,38 @@
---Remhub
-
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Remhub", HidePremium = false, SaveConfig = true, ConfigFolder = "Remhub"})
-
 --values
 
-_G.autoFarm = true
-_G.autoLoad = true
+getgenv().mobs= {}
+getgenv().worlds= {}
+getgenv().autoegg = false
+
+--mobs
+if  game:GetService("Workspace").Snow:FindFirstChild("EnemySpawners") 
+then for _,v in pairs(game:GetService("Workspace").Snow.EnemySpawners:GetChildren()) do
+	table.insert(mobs, v.Name) end
+else
+
+for _,v in pairs(game:GetService("Workspace").Worlds.DBZ.EnemySpawners:GetChildren()) do
+	table.insert(mobs, v.Name) end
+end
+--worlds
+for _,v in pairs(game:GetService("Workspace").Worlds:GetChildren()) do
+table.insert(worlds, v.Name)
+end
+
+--Remhub
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "ACS | Remhub", HidePremium = false, SaveConfig = true, ConfigFolder = "Remhub"})
 
 --functions
-
-function autoFarm()
-    while _G.autoFarm == true do
-        game:GetService("ReplicatedStorage").common.packages._Index:FindFirstChild("sleitnick_knit@1.5.1").knit.Services.EquipmentService.RE.click:FireServer()
-        wait(0.0000000001)
-    end
-end
-
-function autoLoad()
-    while _G.autoLoad == true do
-        game:GetService("ReplicatedStorage").common.packages._Index:FindFirstChild("sleitnick_knit@1.5.1").knit.Services.EquipmentService.RF.AutoLoad:InvokeServer()
-        wait(1)
-    end
-end
-
+function autoegg()
+	while getgenv().autoegg == true	do
+	local args = {
+		[1] = workspace:WaitForChild("Worlds"):WaitForChild("DBZ"):WaitForChild("DBZOrb"),
+		[2] = 1
+	}
+	game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("Orbs"):WaitForChild("OpenOrbs"):FireServer(unpack(args))
+	wait(0.0000000001)
+	end
+	end
 --tabs
 
 local FarmTab = Window:MakeTab({
@@ -32,29 +41,34 @@ local FarmTab = Window:MakeTab({
 	PremiumOnly = false
 })
 
+--dropdowns
+FarmTab:AddDropdown({
+	Name = "Mobs",
+	Default = "1",
+	Options = {mobs[1],mobs[2]},
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+
 --toggles
-
 FarmTab:AddToggle({
-	Name = "Auto Farm",
+	Name = "Auto Open Egg",
 	Default = false,
 	Callback = function(Value)
-		_G.autoFarm = Value
-        autoFarm()
-    end
-})
-FarmTab:AddToggle({
-	Name = "Auto Load",
-	Default = false,
-	Callback = function(Value)
-		_G.autoLoad = Value
-        autoLoad()
-    end
+		getgenv().autoegg = Value
+		autoegg()
+	end    
 })
 
-
-
+	
+	
+	
+	
 
 
 
 --
+
+
 OrionLib:Init()
